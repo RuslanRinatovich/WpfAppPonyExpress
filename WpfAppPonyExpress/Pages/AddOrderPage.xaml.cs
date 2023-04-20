f20377bb842029a265cab46d63741e253da5b069
RuslanR <RuslanR@R.ru>
2023-04-20 09:26:28 +0300

    Order? ticket

---
+++ WpfAppPonyExpress/Pages/AddOrderPage.xaml.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppPonyExpress.Models;
+using WpfAppPonyExpress.Windows;
+
+namespace WpfAppPonyExpress.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddOrderPage.xaml
+    /// </summary>
+    public partial class AddOrderPage : Page
+    {
+        Order _currentOrder;
+        User _currentUser;
+
+        public AddOrderPage()
+        {
+            InitializeComponent();
+            LoadDataAndInit();
+        }
+
+        /// <summary>
+        /// Загрузка и инициализация полей
+        /// </summary>
+        void LoadDataAndInit()
+        {
+
+            _currentOrder = CreateNewOrder();
+            // текущий пользователь
+            _currentUser = Manager.CurrentUser;
+            if (_currentUser != null)
+            {
+                TextBlockOrderNumber.Text = $"Заказ №{_currentOrder.OrderID} на имя " +
+                    $"{ _currentUser.Clients.SingleOrDefault().GetFio}";
+            }
+            else
+            {
+                TextBlockOrderNumber.Text = $"Заказ №{_currentOrder.OrderID}";
+            }
+            //TextBlockTotalCost.Text = $"Итого {:C}";
+            //DateTimeOrderCreateDate.Value = _currentOrder.OrderCreateDate;
+            //OrderDeliveryDate.Text = _currentOrder.OrderDeliveryDate.ToLongDateString();
+            TextBlockOrderGetCode.Text = _currentOrder.GetCode.ToString();
+            DataContext = _currentOrder;
+            ComboPickupPoint.ItemsSource = DataDBEntities.GetContext().PickupPoints.ToList();
+            ComboService.ItemsSource = DataDBEntities.GetContext().Services.ToList();
+            ComboZone.ItemsSource = DataDBEntities.GetContext().Zones.ToList();
+            DataGridRate.ItemsSource = DataDBEntities.GetContext().Rates.OrderBy(p => p.Service.Name).
+                ThenBy(p => p.Zone.Name).
+                ThenBy(p => p.Weight).
+                ThenBy(p => p.Price).ToList();
+
+        }
+
+        static Order CreateNewOrder()
+        {
+            Order order = new Order();
+            order.OrderID = DataDBEntities.GetContext().Orders.Max(p => p.OrderID) + 1; ;
+            order.OrderCreateDate = DateTime.Now;
+            order.OrderStatusID = 1;
+            order.OrderDeliveryDate = DateTime.Now.AddDays(1);
+            Random rnd = new Random();
+            order.GetCode = rnd.Next(100, 1000);
+            return order;
+        }
+
+        private void ComboService_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LoadRate();
+        }
+
+        private void ComboZone_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LoadRate();
+        }
+
+        private void UpDownWeight_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            LoadRate();
+        }
+
+        private void LoadRate()
+        {
+            // получаем текущие данные из бд
+            var currentData = DataDBEntities.GetContext().Rates.OrderBy(p => p.Service.Name).
+                ThenBy(p => p.Zone.Name).
+                ThenBy(p => p.Weight).
+                ThenBy(p => p.Price).ToList();
+            // выбор только тех товаров, по определенному диапазону скидки
+            if (ComboService.SelectedIndex > -1)
+            {
+                currentData = currentData.Where(p => p.ServiceId == (ComboService.SelectedItem as Service).ServiceId).ToList();
+                int days = (ComboService.SelectedItem as Service).DaysCount;
+                DateTime? current = DateTimeOrderCreateDate.Value;
+                current = current.Value.AddDays(days);
+                _currentOrder.OrderDeliveryDate = Convert.ToDateTime(current);
+                DateTimeOrderDeliveryDate.Value = Convert.ToDateTime(current);
+                double maxweight = currentData.Max(p => p.Weight);
+                UpDownWeight.Maximum = maxweight;
+            }
+            if (ComboZone.SelectedIndex > -1)
+                currentData = currentData.Where(p => p.ZoneId == (ComboZone.SelectedItem as Zone).ZoneID).ToList();
+
+
+            if ((UpDownWeight.Value > 0) && (ComboService.SelectedIndex > -1) && (ComboZone.SelectedIndex > -1))
+            {
+                currentData = currentData.Where(p => p.Weight >= UpDownWeight.Value).ToList().GetRange(0, 1);
+            }
+            else
+            if (UpDownWeight.Value > 0)
+            {
+                currentData = currentData.Where(p => p.Weight >= UpDownWeight.Value).ToList();
+            }
+
+            DataGridRate.ItemsSource = currentData;
+        }
+
+        // отображение номеров строк в DataGrid
+        private void DataGridGoodLoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = (e.Row.GetIndex() + 1).ToString();
+        }
+
+        private void BtnOk_Click(object sender, RoutedEventArgs e)
+        {
+            if (Manager.MainFrame.CanGoBack)
+                Manager.MainFrame.GoBack();
+        }
+
+        private void BtnBuyItem_Click(object sender, RoutedEventArgs e)
+        {
+            
+                // если не выбран пункт выдачи, отображаем в сообщение
+                if (ComboPickupPoint.SelectedItem == null)
+                {
+                    MessageBox.Show("не выбран пункт выдачи");
+                    return;
+                }
+
+                MessageBoxResult messageBoxResult = MessageBox.Show($"Оформить покупку???",
+                    "Оформление", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+                if (messageBoxResult == MessageBoxResult.OK)
+                {
+                    // пункт выдачи
+                    _currentOrder.OrderPickupPointID = Convert.ToInt32(ComboPickupPoint.SelectedValue);
+                    // добавляем товар
+                    DataDBEntities.GetContext().Orders.Add(_currentOrder);
+
+                    DataDBEntities.GetContext().SaveChanges();  // Сохраняем изменения в БД
+                                                                    // показываем талон заказа в новом окне 
+                    OrderTicketWindow orderTicketWindow = new OrderTicketWindow(_currentOrder);
+                    orderTicketWindow.ShowDialog();
+                    
+                    
+                }
+           
+
+        }
+    }
+}
---
+++ WpfAppPonyExpress/Windows/OrderTicketWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Word = Microsoft.Office.Interop.Word;
+using WpfAppPonyExpress.Models;
+
+namespace WpfAppPonyExpress.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для OrderTicketWindow.xaml
+    /// </summary>
+    
+        public partial class OrderTicketWindow : Window
+        {
+
+            Order _currentOrder; // текущий заказ
+            User _currentUser;// текущий пользователь
+            public OrderTicketWindow(Order order)
+            {
+                InitializeComponent();
+                LoadDataAndInit(order);
+            }
+            // Загрузка и инициализация данных
+            void LoadDataAndInit(Order order)
+            {
+                _currentOrder = order;
+
+                _currentUser = Manager.CurrentUser;
+                if (_currentUser != null)
+                {
+                TextBlockOrderNumber.Text = $"Заказ №{_currentOrder.OrderID} на имя " +
+                    $"{ _currentUser.Clients.SingleOrDefault().GetFio} оформлен";
+                }
+                else
+                { 
+                    TextBlockOrderNumber.Text = $"Заказ №{_currentOrder.OrderID} оформлен"; 
+                }
+                TextBlockTotalCost.Text = $"Итого {_currentOrder.Rate.Price:C}";
+                TextBlockOrderCreateDate.Text = _currentOrder.OrderCreateDate.ToLongDateString();
+                TextBlockOrderDeliveryDate.Text = _currentOrder.OrderDeliveryDate.ToLongDateString();
+                TextBlockOrderGetCode.Text = _currentOrder.GetCode.ToString();
+                TextBlockPickupPoint.Text = _currentOrder.PickupPoint.Address;
+            }
+
+            private void BtnOk_Click(object sender, RoutedEventArgs e)
+            {
+                this.Close();
+            }
+
+            private void BtnSavePDF_Click(object sender, RoutedEventArgs e)
+            {
+                PrintInPdf(_currentOrder);
+            }
+
+            void PrintInPdf(Order order)
+            {
+                try
+                {
+                    string path = null;
+                    // указываем файл для сохранения
+                    SaveFileDialog saveFileDialog = new SaveFileDialog();
+                    saveFileDialog.Filter = "PDF (.pdf)|*.pdf"; // Filter files by extension
+                                                                // если диалог завершился успешно
+                    if (saveFileDialog.ShowDialog() == true)
+                    {
+                        path = saveFileDialog.FileName;
+                        Word.Application application = new Word.Application();
+                        Word.Document document = application.Documents.Add();
+                        Word.Paragraph paragraph = document.Paragraphs.Add();
+                        Word.Range range = paragraph.Range;
+                        range.Font.Bold = 1;
+                        range.Text = $"Номер заказа: {order.OrderID}";
+                        range.InsertParagraphAfter();
+
+                        range = paragraph.Range;
+                        range.Font.Bold = 0;
+                        range.Text = $"Дата заказа: {order.OrderCreateDate}\n" +
+                            $"Дата получения заказа: {order.OrderDeliveryDate}\n" +
+                            $"Пункт выдачи: {order.PickupPoint.Address}";
+                        range.InsertParagraphAfter();
+
+                        range = paragraph.Range;
+                        range.Font.Bold = 1;
+                        range.Text = $"Код получения: {order.GetCode}";
+                        range.InsertParagraphAfter();
+                        range.Font.Bold = 0;
+
+                       
+                        Word.Paragraph generalSumProduct = document.Paragraphs.Add();
+                        Word.Range generalRange = generalSumProduct.Range;
+                        generalRange.Text = $"\nОбщая стоимость заказа: {order.Rate.Price:f2} руб.";
+                        document.SaveAs2($@"{path}", Word.WdExportFormat.wdExportFormatPDF);
+                        MessageBox.Show("Талон сохранен");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+            // отображение номеров строк в DataGrid
+            private void DataGridGoodLoadingRow(object sender, DataGridRowEventArgs e)
+            {
+                e.Row.Header = (e.Row.GetIndex() + 1).ToString();
+            }
+        }
+    }
